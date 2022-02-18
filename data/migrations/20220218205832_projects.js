d753f6d26aa0b8402602a8481c9ecf39a0e5f197
kevnjs <91511588+kevnjs@users.noreply.github.com>
2022-02-18 13:01:17 -0800

    Setup schema

---
+++ data/migrations/20220218205832_projects.js
@@ -0,0 +1,44 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+ exports.up = function(knex) {
+    return knex.schema.createTable('projects', tbl => {
+        tbl.increments('project_id')
+        tbl.string('project_name').notNullable()
+        tbl.string('project_description')
+        tbl.boolean('project_completed').defaultTo(false)
+    })
+    .createTable('resources', tbl => {
+        tbl.increments('resource_id')
+        tbl.string('resource_name').unique().notNullable()
+        tbl.string('resource_description')
+    })
+    .createTable('tasks', tbl => {
+        tbl.increments('task_id')
+        tbl.string('task_description').notNullable()
+        tbl.string('task_notes')
+        tbl.boolean('task_completed').defaultTo(false)
+        tbl.integer('project_id').notNullable().unsigned()
+        .references('project_id').inTable('projects')
+    })
+    .createTable('project_resources', tbl => {
+        tbl.increments('project_resource_id')
+        tbl.integer('project_id').notNullable().unsigned()
+        .references('project_id').inTable('projects')
+        tbl.integer('resource_id').notNullable().unsigned()
+        .references('resource_id').inTable('resources')
+    })
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function(knex) {
+      return knex.schema.dropTableIfExists('project_resources')
+      .dropTableIfExists('tasks')
+      .dropTableIfExists('resources')
+      .dropTableIfExists('projects')
+  };
+  
